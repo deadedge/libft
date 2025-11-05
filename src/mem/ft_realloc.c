f06f5affce3fbaeba53fe401039c775f8ece41d8
jocas <jocas@gmail.com>
2025-11-05 12:19:00 +0000

    refactor ft_realloc function for clarity and consistency

--- src/mem/ft_realloc.c
+++ src/mem/ft_realloc.c
@@ -3,40 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   ft_realloc.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joao-cor <joao-cor@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jocas <jocas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 14:30:16 by joao-cor          #+#    #+#             */
-/*   Updated: 2025/10/17 14:48:48 by joao-cor         ###   ########.fr       */
+/*   Updated: 2025/11/05 12:18:45 by jocas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-void	*ft_realloc(void *ptr, size_t ptr_size, size_t size)
+void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 {
-	unsigned char	*realloced_ptr;
-	unsigned char	*ptr_bytes;
+	unsigned char	*new_ptr;
+	unsigned char	*old_bytes;
 	size_t			i;
 
-	if (size == 0)
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
+	new_ptr = malloc(new_size);
+	if (!new_ptr)
+		return (NULL);
 	i = 0;
-	realloced_ptr = malloc(size);
-	if (!realloced_ptr)
-		return (NULL);
-	ptr_bytes = (unsigned char *)ptr;
-	if (ptr)
+	old_bytes = (unsigned char *)ptr;
+	while (i < new_size)
 	{
-		while (i < size)
-		{
-			if (i < ptr_size)
-				realloced_ptr[i] = ptr_bytes[i];
-			i++;
-		}
-		free(ptr);
+		new_ptr[i] = '\0';
+		if (ptr && i < old_size)
+			new_ptr[i] = old_bytes[i];
+		i++;
 	}
-	return (realloced_ptr);
+	free(ptr);
+	return (new_ptr);
 }
