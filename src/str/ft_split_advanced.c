7d559d180e3d2b7c07d7990830040f04d1c2d309
joao-cor <joao-cor@student.42lisboa.com>
2025-12-04 15:25:06 +0000

    fixed ft_splited_advnaced

--- src/str/ft_split_advanced.c
+++ src/str/ft_split_advanced.c
@@ -3,65 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split_advanced.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jocas <jocas@student.42.fr>                +#+  +:+       +#+        */
+/*   By: joao-cor <joao-cor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 11:45:29 by joao-cor          #+#    #+#             */
-/*   Updated: 2025/12/02 12:13:41 by jocas            ###   ########.fr       */
+/*   Updated: 2025/12/04 15:24:53 by joao-cor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	find_next_quote(char const *str, int start, int str_len,
-		int actual_state)
-{
-	int	i;
-
-	i = 0;
-	if (actual_state)
-		return (1);
-	while (str[start + i] && start + i < str_len)
-	{
-		if (str[start + i] == '\'' || str[start + i] == '"')
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-static void	free_spaces(char **space)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (space[i])
-	{
-		free(space[i]);
-		i++;
-	}
-	free(space);
-	return ;
-}
-
 static int	count_words(char const *str, char c)
 {
-	int	i;
-	int	str_len;
-	int	count;
-	int	inside_str;
+	int		i;
+	int		str_len;
+	int		count;
+	char	quote;
 
 	i = 0;
 	count = 0;
 	str_len = ft_strlen(str);
-	inside_str = 0;
+	quote = 0;
 	while (str[i] == c)
 		i++;
 	while (i < str_len)
 	{
-		if ((str[i] == '\'' || str[i] == '"') && find_next_quote(str, (i + 1),
-				str_len, inside_str))
-			inside_str = !inside_str;
-		if ((str[i] == c && (i + 1 != str_len)) && !inside_str)
+		if (str[i] == '\'' || str[i] == '"')
+		{
+			if (quote == 0)
+				quote = str[i];
+			else if (quote == str[i])
+				quote = 0;
+		}
+		if (str[i] == c && quote == 0 && i + 1 < str_len)
 			count++;
 		i++;
 	}
@@ -71,22 +44,25 @@ static int	count_words(char const *str, char c)
 static int	fill_line(char const *str, char **line_to_fill, int *last_index,
 		char delimiter)
 {
-	int	start;
-	int	len;
-	int	inside_str;
+	int		start;
+	int		len;
+	char	quote;
 
 	start = *last_index;
 	len = 0;
-	inside_str = 0;
+	quote = 0;
 	while (str[start] == delimiter)
 		start++;
 	while (str[start + len])
 	{
-		if ((str[start + len] == '\'' || str[start + len] == '"')
-				&& find_next_quote(str, start + len + 1, ft_strlen(str),
-				inside_str))
-			inside_str = !inside_str;
-		if (str[start + len] == delimiter && !inside_str)
+		if (str[start + len] == '\'' || str[start + len] == '"')
+		{
+			if (quote == 0)
+				quote = str[start + len];
+			else if (quote == str[start + len])
+				quote = 0;
+		}
+		if (str[start + len] == delimiter && quote == 0)
 			break ;
 		len++;
 	}
@@ -94,8 +70,7 @@ static int	fill_line(char const *str, char **line_to_fill, int *last_index,
 	if (!*line_to_fill)
 		return (0);
 	ft_strlcpy(*line_to_fill, str + start, len + 1);
-	*last_index = start + len + 1;
-	return (1);
+	return ((*last_index = start + len + 1), 1);
 }
 
 char	**ft_split_advanced(char const *s, char c)
@@ -116,10 +91,7 @@ char	**ft_split_advanced(char const *s, char c)
 	while (i < words_number)
 	{
 		if (!fill_line(s, &result[i], &index, c))
-		{
-			free_spaces(result);
-			break ;
-		}
+			return (ft_bi_array_free(result), NULL);
 		i++;
 	}
 	result[i] = NULL;
